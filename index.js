41e7d8ddf6dc50f94e1732d696f4a20ecd6d69fb
manikandansugu <manikandan.sugumar@srmtech.com>
2024-11-19 20:08:46 +0530

    cmd chnage

--- index.js
+++ index.js
@@ -23,19 +23,15 @@ app.get("/api/sent-otp", async (req, res) => {
   try {
     const { phoneNumber } = req.query;
     const otp = Math.floor(100000 + Math.random() * 900000).toString();
-    const response = await axios.get("https://www.fast2sms.com/dev/bulkV2", {
-      params: {
-        authorization:
-          "80ZEaiC9dxbSf5XAUcqzw6sOQenkRyjp2tMV4JDYohur3FmlWBln1CxzckVGA6DOI3ZJf4sXLNbHh5tW",
-        variables_values: otp,
-        route: "otp",
-        numbers: phoneNumber,
-      },
-      headers: {
-        "cache-control": "no-cache",
-      },
-    });
-    if (response) {
+    const response = await axios.get(
+      `https://www.fast2sms.com/dev/bulkV2?authorization=80ZEaiC9dxbSf5XAUcqzw6sOQenkRyjp2tMV4JDYohur3FmlWBln1CxzckVGA6DOI3ZJf4sXLNbHh5tW&route=otp&variables_values=${otp}&flash=0&numbers=${phoneNumber}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (response?.data?.return) {
       res.status(200).json({ success: true, message: "otp sent successfully" });
     }
   } catch (error) {
@@ -43,7 +39,7 @@ app.get("/api/sent-otp", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 9000;
 app.listen(port, "0.0.0.0", () =>
   console.log(`Server running on port ${port}`)
 );
